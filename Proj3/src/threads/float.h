998486bbe841776ca9bae435aa3e15ed7d214df2
tommya98 <tommya98@naver.com>
2022-11-28 09:23:56 +0000

    Proj 3 first commit

---
+++ Proj3/src/threads/float.h
@@ -0,0 +1,61 @@
+// Proj 3 implement
+int int_to_float(int n);
+int float_to_int(int x);
+int float_to_int_round(int x);
+int float_add_float(int x, int y);
+int float_sub_float(int x, int y);
+int float_add_int(int x, int n);
+int float_sub_int(int x, int n);
+int float_mul_float(int x, int y);
+int float_mul_int(int x, int n);
+int float_div_float(int x, int y);
+int float_div_int(int x, int n);
+
+int int_to_float(int n) {
+  return n * (1 << 14);
+}
+
+int float_to_int(int x) {
+  return x / (1 << 14);
+}
+
+int float_to_int_round(int x) {
+  if(x >= 0) {
+    return (x + (1 << 14) / 2) / (1 << 14);
+  }
+  else {
+    return (x - (1 << 14) / 2) / (1 << 14);
+  }
+}
+
+int float_add_float(int x, int y) {
+  return x + y;
+}
+
+int float_sub_float(int x, int y) {
+  return x - y;
+}
+
+int float_add_int(int x, int n) {
+  return x + n * (1 << 14);
+}
+
+int float_sub_int(int x, int n) {
+  return x - n * (1 << 14);
+}
+
+int float_mul_float(int x, int y) {
+  return ((int64_t) x) * y / (1 << 14);
+}
+
+int float_mul_int(int x, int n) {
+  return x * n;
+}
+
+int float_div_float(int x, int y) {
+  return ((int64_t) x) * (1 << 14) / y;
+}
+
+int float_div_int(int x, int n) {
+  return x / n;
+}
